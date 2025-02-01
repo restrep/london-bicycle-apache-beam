3779154135be6eb87c15ced8362228e7be9abccb
Sebastian Restrepo, PhD <66371032+restrep@users.noreply.github.com>
2025-02-01 13:17:19 +0000

    method to calculate distance between stations

--- local_pipeline.py
+++ local_pipeline.py
@@ -19,6 +19,24 @@ cycle_hire_data = [
 ]
 
 
+def calculate_station_distance(stations, cycle_hire):
+
+    station_locations = {s['id']: (s['latitude'], s['longitude']) for s in stations}  # dictionary that will be use to calclate distance between stations
+    
+    for trip in cycle_hire:
+        start_id = trip['start_station_id'] # get ids from the trip
+        end_id = trip['end_station_id']
+        
+        if start_id not in station_locations or end_id not in station_locations: # Quality check: ignore/consider what happens if there is no start or end
+            continue
+        
+        start_location = station_locations[start_id] # this is a tuple with the location
+        end_location = station_locations[end_id] # this is a tuple
+        distance = geodesic(start_location, end_location).kilometers  # Using function from geopy. This was a hint from ML6 from the setup.py
+        
+        yield ((start_id, end_id), distance)  # We use Yield instead of return so that we get a generator not a full list, that wya it works with apache beam
+
+
 # Define the pipeline
 def run_pipeline():
     options = PipelineOptions(
@@ -33,9 +51,14 @@ def run_pipeline():
             | "Create Pairs" >> beam.Map(lambda x: (x['start_station_id'], x['end_station_id']))
             | "Count Rides" >> beam.combiners.Count.PerElement()
             | "Format Ride Output" >> beam.Map(lambda x: f"{x[0][0]},{x[0][1]},{x[1]}")
-            | "Print Ride Counts" >> beam.Map(print)  # print instead of writing to BigQuery
+            | "Print Ride Counts" >> beam.Map(print)  # print instead of writing to BigQuery or push to GCP
         )
 
 
 if __name__ == "__main__":
     run_pipeline()
+
+    station_locations = create_station_location_dict(list(stations_data))
+    print(stations_data)
+    print("#######")
+    print(station_locations)
