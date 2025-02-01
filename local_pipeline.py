0649f9d7222435789c4f4b2b77e81d3f839cadf6
Sebastian Restrepo, PhD <66371032+restrep@users.noreply.github.com>
2025-02-01 12:46:20 +0000

    added local pipeline for counting rides

--- local_pipeline.py
+++ local_pipeline.py
@@ -14,5 +14,28 @@ cycle_hire_data = [
     {'start_station_id': 1, 'end_station_id': 2},
     {'start_station_id': 1, 'end_station_id': 3},
     {'start_station_id': 2, 'end_station_id': 3},
+    {'start_station_id': 3, 'end_station_id': 1},
     {'start_station_id': 3, 'end_station_id': 1}
 ]
+
+
+# Define the pipeline
+def run_pipeline():
+    options = PipelineOptions(
+        runner='DirectRunner'  # this is uset to run locally. On GCP we would use DataFlow
+    )
+    
+    with beam.Pipeline(options=options) as pipeline:
+        
+        ride_counts = (
+            pipeline
+            | "Local Cycle Hire Data" >> beam.Create(cycle_hire_data) # this is used to create data locally
+            | "Create Pairs" >> beam.Map(lambda x: (x['start_station_id'], x['end_station_id']))
+            | "Count Rides" >> beam.combiners.Count.PerElement()
+            | "Format Ride Output" >> beam.Map(lambda x: { 'start_station_id': x[0][0], 'end_station_id': x[0][1], 'ride_count': x[1]})
+            | "Print Ride Counts" >> beam.Map(print)  # print instead of writing to BigQuery
+        )
+
+
+if __name__ == "__main__":
+    run_pipeline()
